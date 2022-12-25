5ed813e4d66e92e498cd568c08d179fb1e8c641b
fatayt <114427129+fatayt@users.noreply.github.com>
2022-12-25 23:27:12 +0300

    Add files via upload

--- src/test/java/com/arcane/Day06_C15_ActionHomeWork.java
+++ src/test/java/com/arcane/Day06_C15_ActionHomeWork.java
@@ -31,11 +31,9 @@ public class Day06_C15_ActionHomeWork {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
     }
-
     @Test
-    public void hoverOver() {
+    public void hoverOver() throws InterruptedException {
 //        1- Given kullanici  https://www.amazon.com/ adresine gider
         driver.get("https://www.amazon.com/");
 
@@ -49,23 +47,33 @@ public class Day06_C15_ActionHomeWork {
         driver.findElement(By.linkText("Account")).click();
 
 //        3- When kullanici “language” actionunda "español-ES" click eder
+
         //1.adim: action objesi olustur
         //Actions actionsHw = new Actions(driver);      //Yukarida olusturuldu
+
         //2.adim: uzerine gidecegimiz (hover Over) elementi locate et
         WebElement actionsHw = driver.findElement(By.id("icp-nav-flyout"));
+
         //3.adim: hover over yap
         actions.moveToElement(actionsHw).perform();
 
         WebElement language = driver.findElement(By.xpath("(//*[@dir='ltr'])[5]"));
+
         // Ardindan eger ES secili degilse radio button'a click ederek secin
         actions.moveToElement(language).perform();
         driver.findElement(By.partialLinkText("español")).click();
+        Thread.sleep(3000);
 
 //        4- Then page title'in “Tu cuenta” icerdigini verify et
-        Assert.assertTrue(driver.findElement(By.partialLinkText("Tu cuenta")).isDisplayed());
 
+        //1.yol: h1 tag name ile locate et
+        WebElement TuCuenta = driver.findElement(By.tagName("h1"));
+        Assert.assertEquals("Tu cuenta", TuCuenta.getText());
+
+        //2.yol:partialLink text ile locate et
+        //Assert.assertTrue(driver.findElement(By.partialLinkText("Tu cuenta")).isDisplayed());
+        // calisiyor ancak aranan Link olmadigi icin bu scripti kullanmadim
     }
-
     @After
     public void tearDown(){
         //close method
